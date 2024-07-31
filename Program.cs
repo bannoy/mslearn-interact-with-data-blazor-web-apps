44e888cea74bda382093cfa38164b79aef4fda43
banno_y <yoshitaka.banno.j7j@jpgr.denso.com>
2024-07-31 12:53:20 +0900

    Unit 5

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
 using BlazingPizza;
+using BlazingPizza.Data;
+using BlazingPizza.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContextFactory<PizzaStoreContext>(opt =>
+    opt.UseSqlite($"Data Source=pizza.db"));
+
+builder.Services.AddSingleton<SpecialsService>();
 
 var app = builder.Build();
 
@@ -17,5 +25,18 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+
+// Initialize the database
+var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+using (var scope = scopeFactory.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+    if (db.Database.EnsureCreated())
+    {
+        SeedData.Initialize(db);
+    }
+}
+
 
 app.Run();
